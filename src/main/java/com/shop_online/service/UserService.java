c6ae79011f12dcb00a4c0ee4c3642d3739751f0c
王冉冉 <1945457312@qq.com>
2023-12-02 13:18:08 +0800

    feat:配置文件修改

--- src/main/java/com/shop_online/service/UserService.java
+++ src/main/java/com/shop_online/service/UserService.java
@@ -1,16 +1,39 @@
-package com.social.service;
+package com.shop_online.service;
 
-import com.social.entity.User;
+import com.shop_online.common.result.PageResult;
+import com.shop_online.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.shop_online.vo.UserVO;
+
+import com.shop_online.common.model.Query;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * <p>
  *  服务类
  * </p>
  *
- * @author ycshang
+ * @author sunyu
  * @since 2023-11-28
  */
 public interface UserService extends IService<User> {
+
+    /**
+     * 用户记录
+     *
+     * @param query
+     * @return
+     */
+
+    PageResult<UserVO> getPage (Query query);
+
+    /**
+     * 导出用户记录
+     *
+     * @param query
+     * @param response
+     */
+    void exportUserInfo(Query query, HttpServletResponse response);
 
 }
